4f00c0aeaeb8a6833db72b6840a327246a6b0407
parkerfromcal <kparker.adrienne@gmail.com>
2021-04-14 18:29:21 -0400

    React router params

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,6 +4,7 @@ import Footer from "./FooterComponent";
 import Directory from "./DirectoryComponent";
 import Home from "./HomeComponent";
 import Contact from "./ContactComponent";
+import CampsiteInfo from "./CampsiteInfo";
 import { Switch, Route, Redirect } from "react-router-dom";
 import { CAMPSITES } from "../shared/campsites";
 import { COMMENTS } from "../shared/comments";
@@ -36,6 +37,21 @@ class Main extends Component {
       );
     };
 
+    const CampsiteWithId = ({ match }) => {
+      return (
+        <CampsiteInfo
+          campsite={
+            this.state.campsites.filter(
+              (campsite) => campsite.id === +match.params.campsiteId
+            )[0]
+          }
+          comments={this.state.comments.filter(
+            (comment) => comment.campsiteId === +match.params.campsiteId
+          )}
+        />
+      );
+    };
+
     return (
       <div>
         <Header />
@@ -46,6 +62,7 @@ class Main extends Component {
             path="/directory"
             render={() => <Directory campsites={this.state.campsites} />}
           />
+          <Route path="/directory/:campsiteId" component={CampsiteWithId} />
           <Route exact path="/contactus" component={Contact} />
           <Redirect to="/home" />
         </Switch>
