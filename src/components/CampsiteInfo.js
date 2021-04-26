b15ade9edadc059857f21090c351a99e2e78e5f6
parkerfromcal <kparker.adrienne@gmail.com>
2021-04-26 12:45:09 -0400

    redux actions

--- src/components/CampsiteInfo.js
+++ src/components/CampsiteInfo.js
@@ -49,8 +49,13 @@ class CommentForm extends Component {
   }
 
   handleSubmit(values) {
-    console.log("These are the values you submitted:" + JSON.stringify(values));
-    alert("These are the values you submitted:" + JSON.stringify(values));
+    this.toggleModal();
+    this.props.addComment(
+      this.props.campsiteId,
+      values.rating,
+      values.author,
+      values.text
+    );
   }
 
   render() {
@@ -137,7 +142,7 @@ class CommentForm extends Component {
   }
 }
 
-function RenderComments({ comments }) {
+function RenderComments({ comments, addComment, campsiteId }) {
   if (comments) {
     return (
       <div className="col-md-5 m-1">
@@ -157,7 +162,7 @@ function RenderComments({ comments }) {
             </div>
           );
         })}
-        <CommentForm />
+        <CommentForm campsiteId={campsiteId} addComment={addComment} />
       </div>
     );
   } else {
@@ -183,7 +188,11 @@ function CampsiteInfo(props) {
         </div>
         <div className="row">
           <RenderCampsite campsite={props.campsite} />
-          <RenderComments comments={props.comments} />
+          <RenderComments
+            comments={props.comments}
+            addComment={props.addComment}
+            campsiteId={props.campsite.id}
+          />
         </div>
       </div>
     );
