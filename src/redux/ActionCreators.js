0bc386ecb88ed6ce18c8239c55d60628af52fc01
parkerfromcal <kparker.adrienne@gmail.com>
2021-05-01 11:29:04 -0400

    adding partners actions and requests

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -159,3 +159,43 @@ export const postComment = (campsiteId, rating, author, text) => (dispatch) => {
       alert("Your comment could not be posted\nError: " + error.message);
     });
 };
+
+export const fetchPartners = () => (dispatch) => {
+  dispatch(partnersLoading());
+
+  return fetch(baseUrl + "partners")
+    .then(
+      (response) => {
+        if (response.ok) {
+          return response;
+        } else {
+          const error = new Error(
+            `Error ${response.status}: ${response.statusText}`
+          );
+          error.response = response;
+          throw error;
+        }
+      },
+      (error) => {
+        const errMess = new Error(error.message);
+        throw errMess;
+      }
+    )
+    .then((response) => response.json())
+    .then((partners) => dispatch(addPartners(partners)))
+    .catch((error) => dispatch(partnersFailed(error.message)));
+};
+
+export const partnersLoading = () => ({
+  type: ActionTypes.PARTNERS_LOADING,
+});
+
+export const partnersFailed = (errMess) => ({
+  type: ActionTypes.PARTNERS_FAILED,
+  payload: errMess,
+});
+
+export const addPartners = (partners) => ({
+  type: ActionTypes.ADD_PARTNERS,
+  payload: partners,
+});
